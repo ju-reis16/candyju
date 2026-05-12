e9e86e0db4b4dde6a2bd8b89824a89f5231b4d5c
ju-reis16 <ju.reis1026@gmail.com>
2026-05-12 13:48:17 -0300

    Adiciona estrutura inicial do componente App

--- App.js
+++ App.js
@@ -15,3 +15,13 @@ const LIGHT_PINK = '#fff0f6';
 const DARK_PINK = '#d63384';
 const WHITE = '#FFFFFF';
 const GRAY = '#666666';
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" />
+
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <View style={styles.card}>
+
+          <Text style={styles.title}>CandyJu 🍭</Text>
+          
