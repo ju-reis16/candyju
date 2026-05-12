209500eb6f09741240c2a8cd643a7a6dafd66be8
ju-reis16 <ju.reis1026@gmail.com>
2026-05-12 13:47:26 -0300

    Adiciona constantes de cores ao App.js

--- App.js
+++ App.js
@@ -9,3 +9,9 @@ import {
   ScrollView,
   Image,
 } from 'react-native';
+
+const PINK = '#ff69b4';
+const LIGHT_PINK = '#fff0f6';
+const DARK_PINK = '#d63384';
+const WHITE = '#FFFFFF';
+const GRAY = '#666666';
