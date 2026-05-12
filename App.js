47f936c5b71fa773f1a75e3dca0d34d8e1a8d5dd
ju-reis16 <ju.reis1026@gmail.com>
2026-05-12 13:47:01 -0300

    Adiciona importações iniciais no App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+  Image,
+} from 'react-native';
